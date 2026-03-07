f3f3742ac5064d690bfdae65bf461c64de0720f8
tramtran-helen <tramtran.helen@gmail.com>
2026-03-07 15:53:58 -0800

    Render SDL window

--- src/Game.cpp
+++ src/Game.cpp
@@ -4,6 +4,7 @@
 
 
 Game::Game() {
+    isRunning = false;
     std::cout << "Game constructor called!" << std::endl;
 }
 
@@ -17,7 +18,7 @@ void Game::Initialize() {
         return;
     };
 
-    SDL_Window* window = SDL_CreateWindow(
+    window = SDL_CreateWindow(
         "Tram's Game Engine", 
         SDL_WINDOWPOS_CENTERED, 
         SDL_WINDOWPOS_CENTERED,
@@ -31,16 +32,18 @@ void Game::Initialize() {
         return;
     } 
 
-    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
+    renderer = SDL_CreateRenderer(window, -1, 0);
 
     if (!renderer) {
         std::cerr << "Error creating SDL renderer" << std::endl;
         return;
     }
+
+    isRunning = true;
 }
 
 void Game::Run() {
-    while(true) {
+    while(isRunning) {
         ProcessInput();
         Update();
         Render();
@@ -48,17 +51,36 @@ void Game::Run() {
 }
 
 void Game::ProcessInput() {
-
+    SDL_Event sdlEvent;
+    while (SDL_PollEvent(&sdlEvent)) {
+        switch(sdlEvent.type) {
+            case SDL_QUIT:
+                isRunning = false;
+                break;
+            case SDL_KEYDOWN:
+                if (sdlEvent.key.keysym.sym == SDLK_ESCAPE) {
+                    isRunning = false;
+                }
+                break;
+        }
+    };
 }
 
 void Game::Update() {
-
+    // Update game objects
 }
 
 void Game::Render() {
+    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+    SDL_RenderClear(renderer);
 
+    // Render game objects
+
+    SDL_RenderPresent(renderer);
 }
 
 void Game::Destroy() {
-
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
 }
--- src/Game.h
+++ src/Game.h
@@ -1,8 +1,13 @@
 #ifndef GAME_H
 #define GAME_H
 
+#include <SDL.h>
+
 class Game {
     private:
+        bool isRunning;
+        SDL_Window* window;
+        SDL_Renderer* renderer;
 
     public:
         Game();
