8cbec0ddfbb31a42795ee9c510dd3888bcc389b3
tramtran-helen <tramtran.helen@gmail.com>
2026-03-08 20:20:53 -0700

    Implement Logger class

--- src/Game.cpp
+++ src/Game.cpp
@@ -1,4 +1,5 @@
 #include "Game.h"
+#include "Logger.h"
 #include <SDL.h>
 #include <SDL_image.h>
 #include <glm/glm.hpp>
@@ -7,16 +8,16 @@
 
 Game::Game() {
     isRunning = false;
-    std::cout << "Game constructor called!" << std::endl;
+    Logger::Log("Game constructor called!");
 }
 
 Game::~Game() {
-    std::cout << "Game destructor called!" << std::endl;
+    Logger::Log("Game destructor called!");
 }
 
 void Game::Initialize() {
     if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
-        std::cerr << "Error initializing SDL." << std::endl;
+        Logger::Err("Error initializing SDL.");
         return;
     };
 
@@ -33,14 +34,14 @@ void Game::Initialize() {
     );
 
     if (!window) {
-        std::cerr << "Error creating SDL window" << std::endl;
+        Logger::Err("Error creating SDL window");
         return;
     } 
 
     renderer = SDL_CreateRenderer(window, -1, 0);
 
     if (!renderer) {
-        std::cerr << "Error creating SDL renderer" << std::endl;
+        Logger::Err("Error creating SDL renderer");
         return;
     }
 
